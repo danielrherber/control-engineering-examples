5d8d64796345e43f8083e3e9120f8a81e5134368
Daniel R. Herber <daniel.herber@colostate.edu>
2023-01-18 16:01:59 -0700

    Updated ex_tf_bode.m

--- 1-introduction/ex_tf_bode.m
+++ 1-introduction/ex_tf_bode.m
@@ -1,21 +1,34 @@
+% ex_tf_bode.m
+% transfer function and bode plot of Example 2.1. RLC Circuit in LSC
 close all; clear; clc
 
-% numerator polynomial coefficients
-NUM = [1];
+% numerator polynomial coefficients (e.g., 3s^2-4s+5 -> [3 -4 5])
+num = [1];
 
-% denominator polynomial coefficients
-DEN = [1 1 1];
+% denominator polynomial coefficients (e.g., 7s^2+8s-9 -> [7 8 -9])
+den = [1 1 1];
 
-% transfer function
-SYS = tf(NUM,DEN)
+% transfer function object
+sys = tf(num,den)
 
 % Bode frequency response of dynamic systems
-bode(SYS)
+bodeplot(sys,'k')
+
+% note the relationship between magnitude and decibels
+title('dB = 20*log10(Mag)')
+
+%--------------------------------------------------------------------------
+% compute magnitude and phase at a specific frequency input
+%--------------------------------------------------------------------------
+% frequency of interest
+w = 3;
+
+% compute response magnitude and phase in degrees at w
+[mag,phase] = bode(sys,w)
 
 % transfer function
 G = @(s) 1/(s^2 + s + 1);
 
 % manually compute magnitude and phase shift at specific w
-w = 3;
-abs(G(i*w))
-rad2deg(angle(G(i*w)))
+abs(G(1i*w))
+rad2deg(angle(G(1i*w)))
