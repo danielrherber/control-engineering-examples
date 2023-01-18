aae6ee11dfb3fa8312e8e22d62b263f8d3bb3362
Daniel R. Herber <daniel.herber@colostate.edu>
2023-01-18 13:46:44 -0700

    Updated ex_linearization.m

--- 1-introduction/ex_linearization.m
+++ 1-introduction/ex_linearization.m
@@ -1,3 +1,6 @@
+% ex_linearization.m
+% simulation of nonlinear and linearized model in Example 2.7. Rocket
+% with Air Resistance in LSC
 close all; clear; clc
 
 % assumed parameters
@@ -10,11 +13,11 @@ T0 = 3;
 V0 = sqrt((T0-m*g)/b);
 
 % user-defined thrust input
-T = @(t) T0 + (t>=30)*2*T0 - (t>=60)*2.25*T0;
-% T = @(t) T0 + sin(t);
+% T = @(t) T0 + (t>=30)*2*T0 - (t>=60)*2.25*T0;
+T = @(t) T0 + sin(t); % <- also try this
 
 % linear matrices
-A = -2*b/m*V0; B = [1/m];
+A = -2*b/m*V0; B = 1/m;
 
 % nonlinear state derivative function
 fnonlinear = @(t,v) -b/m*sign(v)*v^2 + T(t)/m - g;
@@ -23,26 +26,50 @@ fnonlinear = @(t,v) -b/m*sign(v)*v^2 + T(t)/m - g;
 flinear = @(t,vdelta) A*vdelta + B*(T(t)-T0);
 
 % time horizon bounds
-TSPAN = [0 100];
+tspan = [0 100];
 
 % initial state
-Y0 = 0.5;
+X0 = 0.5;
 
 % ode options (more accurate simulation)
 OPTIONS = odeset('Reltol',1e-12);
 
-% shift linear simulation initial value
-Y0linear = Y0 - V0;
-
 % nonlinear simulation
-[Tnon,Ynon] = ode45(@(t,v) fnonlinear(t,v),TSPAN,Y0,OPTIONS);
+[T_nonlin,X_nonlin] = ode45(@(t,v) fnonlinear(t,v),tspan,X0,OPTIONS);
+
+% shift linear simulation initial value
+X0linear = X0 - V0;
 
 % linear simulation
-[Tlin,Ylin] = ode45(@(t,v) flinear(t,v),TSPAN,Y0linear,OPTIONS);
+[T_lin,X_lin] = ode45(@(t,v) flinear(t,v),tspan,X0linear,OPTIONS);
 
-% plot
-figure; hold on
-plot(TSPAN,[V0 V0],'--')
-plot(Tnon,Ynon)
-plot(Tlin,Ylin+V0) % needs offset
-legend('nonlinear','linear')
+% plot the simulation results (see function below)
+% note the X_lin offset
+plot_example(tspan,V0,T_nonlin,X_nonlin,T_lin,X_lin+V0)
+
+%--------------------------------------------------------------------------
+% plotting code (not the main content)
+%--------------------------------------------------------------------------
+function plot_example(TSPAN,V0,T_nonlin,X_nonlin,T_lin,X_lin)
+
+% colors
+niceblue = [77, 121, 167]/255;
+nicered = [225, 86, 86]/255;
+% nicegreen = [109, 195, 80]/255;
+% nicegray = [242, 242, 242]/255;
+xmediumgray = [170, 170, 170]/255;
+
+% initialize figure
+hf = figure; hf.Color = 'w'; hold on
+LineWidth = 1;
+
+plot(TSPAN,[V0 V0],'--','Color',xmediumgray,'LineWidth',LineWidth,'DisplayName','V0');
+plot(T_nonlin,X_nonlin,'Color',niceblue,'LineWidth',LineWidth,'DisplayName','Nonlinear Model')
+plot(T_lin,X_lin,'Color',nicered,'LineWidth',LineWidth,'DisplayName','Linearized Model')
+
+xlabel('Time [sec]')
+ylabel('State v')
+
+legend();
+
+end
