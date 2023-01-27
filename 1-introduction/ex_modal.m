483a386d7bc1e5eb156b5f6ca482b2e75fe6c553
Daniel R. Herber <daniel.herber@colostate.edu>
2023-01-26 22:25:03 -0700

    Updated ex_modal.m

--- 1-introduction/ex_modal.m
+++ 1-introduction/ex_modal.m
@@ -1,5 +1,6 @@
+% ex_modal.m
+% simulation of a 2nd-order damped oscillator in modal canonical form
 close all; clear; clc
-hf = figure; hf.Color = 'w'; hold on
 
 % LTI matrix
 A = @(w0,zeta) [0 1; -w0^2 -2*zeta*w0];
@@ -8,41 +9,89 @@ A = @(w0,zeta) [0 1; -w0^2 -2*zeta*w0];
 f = @(t,y,w0,zeta) A(w0,zeta)*y;
 
 % initial condition
-Y0 = [1 -1];
+X0 = [1 -1];
+% X0 = [1 0]; % <- also try this
 
 % time horizon
 TSPAN = [0 15];
 
-% test 1
-w0 = 1; zeta = 2;
-[T1,Y1] = ode45(@(t,y) f(t,y,w0,zeta),TSPAN,Y0);
-plot(T1,Y1,'r','linewidth',2);
+% simulation options
+OPTIONS = odeset('RelTol',1e-8,'AbsTol',1e-8);
 
-% test 2
-w0 = 1; zeta = 0.2;
-[T2,Y2] = ode45(@(t,y) f(t,y,w0,zeta),TSPAN,Y0);
-plot(T2,Y2,'b','linewidth',2);
+%--------------------------------------------------------------------------
+% different zeta (damping) values
+%--------------------------------------------------------------------------
+w0 = 1;
+ZETA = [2 1 0.4 0];
 
-% test 3
-w0 = 1; zeta = 0;
-[T3,Y3] = ode45(@(t,y) f(t,y,w0,zeta),TSPAN,Y0);
-plot(T3,Y3,'g','linewidth',2);
+% initialize plot
+[colors] = plot_example(length(ZETA));
 
-% test 4
-w0 = 1; zeta = 1;
-[T4,Y4] = ode45(@(t,y) f(t,y,w0,zeta),TSPAN,Y0);
-plot(T4,Y4,'k','linewidth',2);
+% go through each value of zeta
+for idx = 1:length(ZETA)
 
-% test 5
-w0 = 1; zeta = -0.01;
-[T5,Y5] = ode45(@(t,y) f(t,y,w0,zeta),TSPAN,Y0);
-plot(T5,Y5,'m','linewidth',2);
+    % extract
+    zeta = ZETA(idx);
 
-%% phase plot
+    % run simulation
+    [T,X] = ode45(@(t,x) f(t,x,w0,ZETA(idx)),TSPAN,X0,OPTIONS);
+
+    % add result to figure
+    plot_example_add(T,X,colors(idx,:),zeta,"\zeta")
+
+end
+
+%--------------------------------------------------------------------------
+% different w0 (frequency) values
+%--------------------------------------------------------------------------
+W0 = [1/4, 1/2, 1, 2, 4];
+zeta = 0.4;
+
+% initialize plot
+colors = plot_example(length(W0));
+
+% go through each value of w0
+for idx = 1:length(W0)
+
+    % extract
+    w0 = W0(idx);
+
+    % run simulation
+    [T,X] = ode45(@(t,x) f(t,x,w0,zeta),TSPAN,X0,OPTIONS);
+
+    % add result to figure
+    plot_example_add(T,X,colors(idx,:),w0,"\omega_0")
+
+end
+
+%--------------------------------------------------------------------------
+% plotting code (not the main content)
+%--------------------------------------------------------------------------
+% initialize
+function [colors] = plot_example(N)
+
 hf = figure; hf.Color = 'w'; hold on
-xlabel('x1'); ylabel('x2')
-plot(Y1(:,1),Y1(:,2),'r','linewidth',2)
-plot(Y2(:,1),Y2(:,2),'b','linewidth',2)
-plot(Y3(:,1),Y3(:,2),'g','linewidth',2)
-plot(Y4(:,1),Y4(:,2),'k','linewidth',2)
-plot(Y5(:,1),Y5(:,2),'m','linewidth',2)
+axis equal
+colors = lines(N);
+
+end
+% add a single simulation
+function plot_example_add(T,X,color,value,type)
+
+% plot state space
+subplot(1,3,1); hold on; legend();
+axis equal; xlabel('State 1'); ylabel('State 2');
+plot(X(:,1),X(:,2),'Color',color,'linewidth',2,...
+    'DisplayName',strcat(type," = ",string(value)))
+
+% plot state 1 trajectory
+subplot(1,3,2); hold on
+xlabel('Time [sec]'); ylabel('State 1');
+plot(T,X(:,1),'Color',color,'linewidth',2);
+
+% plot state 2 trajectory
+subplot(1,3,3); hold on
+xlabel('Time [sec]'); ylabel('State 2');
+plot(T,X(:,2),'Color',color,'linewidth',2);
+
+end
