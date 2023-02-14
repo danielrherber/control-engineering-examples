468943dbb8bbcbd64904385133c9ab8fffe0f086
Daniel R. Herber <daniel.herber@colostate.edu>
2023-02-13 23:57:52 -0700

    Updated ex_full_state_feedback.m with time constant info

--- 2-linear-control-design/ex_full_state_feedback.m
+++ 2-linear-control-design/ex_full_state_feedback.m
@@ -11,16 +11,16 @@ rank(Co)==length(A)
 
 % desired eigenvalues
 epsilon = [0 1e-4 -1e-4];
-E1 = [-0.67 -0.67 -0.67]+epsilon;
-E2 = [-1 -1 -1]+epsilon;
-E3 = [-0.33 -0.33 -0.33]+epsilon;
-E4 = [-4 -4 -4]+epsilon;
+tc = 1.5;   E1p5 = [-1/tc -1/tc -1/tc]+epsilon;
+tc = 1;     E1 = [-1/tc -1/tc -1/tc]+epsilon;
+tc = 3;     E3 = [-1/tc -1/tc -1/tc]+epsilon;
+tc = 0.125; E0p125 = [-1/tc -1/tc -1/tc]+epsilon;
 
 % closed-loop pole assignment using state feedback
-[K1,PREC1] = place(A,B,E1);
-[K2,PREC2] = place(A,B,E2);
+[K1p5,PREC1] = place(A,B,E1p5);
+[K1,PREC2] = place(A,B,E1);
 [K3,PREC3] = place(A,B,E3);
-[K4,PREC4] = place(A,B,E4);
+[K0p125,PREC4] = place(A,B,E0p125);
 
 % control law
 u = @(x,K,r) -K*x + r;
@@ -30,46 +30,48 @@ f = @(t,x,A,B,K,r) A*x + B*u(x,K,r);
 
 % simulation options
 TSPAN = [0 30];
-x0 = [1;1;1];
+X0 = [1;1;1];
 OPTIONS = odeset('RelTol',1e-13);
 
 % reference input
 r = 0;
 
 % run the simulations
-[T0,X0] = ode45(@(t,x) f(t,x,A,B,0*K1,r),TSPAN,x0,OPTIONS);
-[T1,X1] = ode45(@(t,x) f(t,x,A,B,K1,r),TSPAN,x0,OPTIONS);
-[T2,X2] = ode45(@(t,x) f(t,x,A,B,K2,r),TSPAN,x0,OPTIONS);
-[T3,X3] = ode45(@(t,x) f(t,x,A,B,K3,r),TSPAN,x0,OPTIONS);
-[T4,X4] = ode45(@(t,x) f(t,x,A,B,K4,r),TSPAN,x0,OPTIONS);
+[T,X] = ode45(@(t,x) f(t,x,A,B,0*K1p5,r),TSPAN,X0,OPTIONS);
+[T1p5,X1p5] = ode45(@(t,x) f(t,x,A,B,K1p5,r),TSPAN,X0,OPTIONS);
+[T1,X1] = ode45(@(t,x) f(t,x,A,B,K1,r),TSPAN,X0,OPTIONS);
+[T3,X3] = ode45(@(t,x) f(t,x,A,B,K3,r),TSPAN,X0,OPTIONS);
+[T0p125,X0p125] = ode45(@(t,x) f(t,x,A,B,K0p125,r),TSPAN,X0,OPTIONS);
 
 % states
 hf = figure; hf.Color = 'w'; hold on
-plot(T0,X0,'k')
-plot(T1,X1,'r')
-plot(T2,X2,'g')
-plot(T4,X4,'m')
+plot(T,X,'k','DisplayName','original')
+plot(T0p125,X0p125,'m','DisplayName',' 0.125 s')
+plot(T1,X1,'g','DisplayName','1 s')
+plot(T1p5,X1p5,'r','DisplayName','1.5 s')
+plot(T3,X3,'b','DisplayName','3 s')
 xlim([0 30]); ylim([-20 10])
 xlabel('time [sec]'); ylabel('x(t)')
-
-% outputs
-hf = figure; hf.Color = 'w'; hold on
-plot(T0,C*X0','k')
-plot(T1,C*X1','r')
-plot(T2,C*X2','g')
-plot(T3,C*X3','b')
-plot(T4,C*X4','m')
-xlim([0 30]); ylim([-6 3])
-xlabel('time [sec]'); ylabel('y(t)')
-legend('original','-0.67','-1','-0.33','-4')
+legend();
 
 % control
 hf = figure; hf.Color = 'w'; hold on
-plot(T0,0*K1*X0' + r,'k')
-plot(T1,K1*X1' + r,'r')
-plot(T2,K2*X2' + r,'g')
-plot(T3,K3*X3' + r,'b')
-plot(T4,K4*X4' + r,'m')
+plot(T,0*K1p5*X' + r,'k','DisplayName','original')
+plot(T0p125,K0p125*X0p125' + r,'m','DisplayName',' 0.125 s')
+plot(T1,K1*X1' + r,'g','DisplayName','1 s')
+plot(T1p5,K1p5*X1p5' + r,'r','DisplayName','1.5 s')
+plot(T3,K3*X3' + r,'b','DisplayName','3 s')
 xlim([0 30]); ylim([-200 200])
 xlabel('time [sec]'); ylabel('u(t)')
-legend('original','-0.67','-1','-0.33','-4')
+legend();
+
+% outputs
+hf = figure; hf.Color = 'w'; hold on
+plot(T,C*X','k','DisplayName','original')
+plot(T0p125,C*X0p125','m','DisplayName',' 0.125 s')
+plot(T1,C*X1','g','DisplayName','1 s')
+plot(T1p5,C*X1p5','r','DisplayName','1.5 s')
+plot(T3,C*X3','b','DisplayName','3 s')
+xlim([0 30]); ylim([-6 3])
+xlabel('time [sec]'); ylabel('y(t)')
+legend();
