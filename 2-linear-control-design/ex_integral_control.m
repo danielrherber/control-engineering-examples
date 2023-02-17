aeac1bc0db8bba1086d22549a6a668787f6da035
Daniel Herber <daniel.r.herber@gmail.com>
2023-02-17 15:36:26 -0700

    Updated ex_integral_control.m

--- 2-linear-control-design/ex_integral_control.m
+++ 2-linear-control-design/ex_integral_control.m
@@ -1,3 +1,7 @@
+% ex_integral_control.m
+% design and simulation of a full state feedback controller with
+% output error integrators from Example 4.12 Integral Control of a Third
+% Order System in LSC
 close all; clear; clc
 
 % augmented system matrices
@@ -25,59 +29,50 @@ rank(Mc);
 
 % desired eigenvalues
 epsilon = 1e-3;
-E1 = [-0.67 -0.67 -0.67 -0.67] - [0 epsilon 2*epsilon 3*epsilon];
+tc = 1.5; E1 = [-1/tc -1/tc -1/tc -1/tc] - [0 epsilon 2*epsilon 3*epsilon];
 
 % closed-loop pole assignment using state feedback
-K1 = place(A1,B1,E1);
+K1 = place(A1,B1,E1); % with output error integrator
 K = place(A,B,E1(1:3));
 
 % create the closed-loop state-space models
-sys1 = ss(A1-B1*K1,Br1,C1,[]);
+sys1 = ss(A1-B1*K1,Br1,C1,[]); % with output error integrator
 sys = ss(A-B*K,Br,C,[]);
 
 % simulations options
-X0 = [1 1 1 0]';
+X0 = [1 1 1 0]'; % integrator state initialized to 0
 tfinal = 100;
 T = linspace(0,tfinal,10000)';
 
 % reference signals
-% r = -3*ones(size(T));
+% r = -3*ones(size(T)); % <- also try this
 r = floor(sin(0.1*T));
-% r = 0.1*T;
-% r = sin(0.3*T);
-% x = 1;
-% r = 2*T.^x; % note: error increases with x > 1
+% r = 0.1*T; % <- also try this
+% r = sin(0.3*T); % <- also try this
+% x = 1; % <- also try this
+% r = 2*T.^x;  % <- also try this % note: error increases with x > 1
 
 % simulate
-Y1 = lsim(sys1,r,T,X0);
+Y1 = lsim(sys1,r,T,X0); % with output error integrator
 Y = lsim(sys,r,T,X0(1:3));
 
 % plot outputs and reference
-figure; hold on
-plot(T,Y1)
-plot(T,Y)
-plot(T,r)
-legend('Y1','Y','r')
+hf = figure; hf.Color = 'w'; hold on
+plot(T,[Y1,Y,r],'LineWidth',2)
+xlabel('Time [sec]'); ylabel('Signals');
+legend('Output with integrator','Output without integrator','Reference')
+title('Output tracking without a disturbance')
 
-% plot error
-figure; hold on
-plot(T,abs(r-Y1))
-ha = gca; ha.YScale = 'log';
-
-%% non-zero disturbance
-% create the closed-loop state-space models
+% create the closed-loop state-space models for a non-zero disturbance
 sys1v = ss(A1-B1*K1,[Br1,Bv1],C1,[]);
 sysv = ss(A-B*K,[Br,Bv],C,[]);
 
-% reference signals
-r = 1*ones(size(T));
-
 % disturbance
-% v = -10*ones(size(T));
-v = 0.1*floor(sin(0.1*T));
-% v = 0.1*sin(0.1*T);
+% v = -3*ones(size(T)); % <- also try this
+% v = 0.1*floor(sin(0.1*T)); % <- also try this
+v = 0.1*sin(8*T);
 
-% inputs
+% inputs (reference + disturbance)
 u = [r,v];
 
 % simulate
@@ -85,14 +80,8 @@ Y1v = lsim(sys1v,u,T,X0);
 Yv = lsim(sysv,u,T,X0(1:3));
 
 % plot outputs and inputs
-figure; hold on
-plot(T,Y1v)
-plot(T,Yv)
-plot(T,r)
-plot(T,v)
-legend('Y1','Y','r','v')
-
-% bode with for reference and disturbance
-figure
-sys1v = ss(A1-B1*K1,Bv1,C1,[]);
-bode(sys1v,'r')
+hf = figure; hf.Color = 'w'; hold on
+plot(T,[Y1v,Yv,r,v],'LineWidth',2)
+xlabel('Time [sec]'); ylabel('Signals');
+legend('Output with integrator','Output without integrator','Reference','Disturbance')
+title('Output tracking with a disturbance')
