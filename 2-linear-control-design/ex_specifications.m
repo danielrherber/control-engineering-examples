6e4066cca11c3b1c4f6587d491b42351844a2467
Daniel Herber <daniel.r.herber@gmail.com>
2023-02-27 16:52:06 -0700

    Updated ex_specifications.m

--- 2-linear-control-design/ex_specifications.m
+++ 2-linear-control-design/ex_specifications.m
@@ -1,3 +1,7 @@
+% ex_specifications.m
+% illustration of the time and frequency domain specifications using Matlab
+% functions stepinfo, dcgain, allmargin, and loopsens as well as Nyquist
+% diagrams
 close all; clear; clc
 
 % system matrices
@@ -8,11 +12,13 @@ C = [2 0 0];
 % state-space model
 sys = ss(A,B,C,[]);
 
-% Rise time, settling time, and other step-response characteristics
+% rise time, settling time, and other step-response characteristics
 S = stepinfo(sys)
 k = dcgain(sys)
 
-%% full state feedback
+%--------------------------------------------------------------------------
+% full state feedback
+%--------------------------------------------------------------------------
 % desired eigenvalues
 epsilon = [0 1e-4 -1e-4];
 E = [-0.67 -0.67 -0.67]+epsilon;
@@ -23,10 +29,13 @@ K = place(A,B,E);
 % closed-loop system
 sys1 = ss(A-B*K,B,C,[]);
 
+% rise time, settling time, and other step-response characteristics
 S1 = stepinfo(sys1)
 k1 = dcgain(sys1)
 
-%% full state feedback with integrator
+%--------------------------------------------------------------------------
+% full state feedback with integrator
+%--------------------------------------------------------------------------
 % augmented system matrices
 Ai = [-0.14 0.33 -0.33 0;
     0.1 -0.28 0 0;
@@ -47,23 +56,37 @@ Ki = place(Ai,Bi,Ei);
 % create the closed-loop state-space models
 sysi = ss(Ai-Bi*Ki,Bri,Ci,[]);
 
+% rise time, settling time, and other step-response characteristics
 Si = stepinfo(sysi)
 ki = dcgain(sysi)
 
-%% Gain margin, phase margin, and crossover frequencies
-rng(4456434)
-P = rss(4);
-[Gm,Pm,Wcg,Wcp] = margin(P)
+%--------------------------------------------------------------------------
+% gain margin, phase margin, and crossover frequencies
+%--------------------------------------------------------------------------
+rng(6848)
+% rng(4456434) % <- also try this
+P = rss(4); % generate a randomized continuous-time state-space model
+Sm = allmargin(P)
 
-figure;
+% plot Bode frequency response
+hf = figure; hf.Color = 'w';
 bode(P)
 
-%% Sensitivity functions of plant-controller feedback loop
+% plot Nyquist frequency response
+hf = figure; hf.Color = 'w';
+nyquist(P)
+
+%--------------------------------------------------------------------------
+% sensitivity functions of plant-controller feedback loop
+%--------------------------------------------------------------------------
+% create a state-space model
 rng(242)
 P = rss(6);
 C = rss(3);
 
+% determine sensitivity functions of plant-controller feedback loop
 SF = loopsens(P,C)
 
-figure;
+% plot sensitivity at the plant input
+hf = figure; hf.Color = 'w';
 bode(SF.Si)
