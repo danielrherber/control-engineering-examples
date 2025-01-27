1fa2d4259cf6d29a7f21932e2b336e8358117638
Daniel R. Herber <daniel.herber@colostate.edu>
2025-01-26 19:31:54 -0700

    Updates to ex_tf_bode.m

--- 1-introduction/ex_tf_bode.m
+++ 1-introduction/ex_tf_bode.m
@@ -1,5 +1,7 @@
 % ex_tf_bode.m
-% transfer function and bode plot of Example 2.1 RLC Circuit in LSC
+% transfer function and bode plot of an electrical RLC circuit
+% [reference] Example 2.1 RLC Circuit in LSC
+% [course] Session 1 - Modeling for Control Design
 close all; clear; clc
 
 % numerator polynomial coefficients (e.g., 3s^2-4s+5 -> [3 -4 5])
@@ -13,6 +15,9 @@ sys = tf(num,den)
 
 % Bode frequency response of dynamic systems
 bodeplot(sys,'k')
+
+% format the plot (see function below)
+plot_example
 
 % note the relationship between magnitude and decibels
 title('dB = 20*log10(Mag)')
@@ -32,3 +37,40 @@ G = @(s) 1/(s^2 + s + 1);
 % manually compute magnitude and phase shift at specific w
 abs(G(1i*w))
 rad2deg(angle(G(1i*w)))
+
+%--------------------------------------------------------------------------
+% plotting code
+% (not the main content)
+function plot_example
+
+% colors and other parameters
+nicered = [225, 86, 86]/255;
+LineWidth = 1;
+FontSize = 12;
+
+% figure
+hf = gcf; hf.Color = 'w';
+
+% axis handles
+ha = gca; ha1 = ha.Children(1); ha2 = ha.Children(2);
+
+% magnitude plot
+ha1.LineWidth = LineWidth;
+ha1.XColor = 'k';
+ha1.YColor = 'k';
+ha1.FontSize = FontSize;
+ha1.Children(1).LineWidth = LineWidth;
+ha1.Children(1).Color = nicered;
+
+% phase plot
+ha2.LineWidth = LineWidth;
+ha2.XColor = 'k';
+ha2.YColor = 'k';
+ha2.FontSize = FontSize;
+ha2.Children(1).LineWidth = LineWidth;
+ha2.Children(1).Color = nicered;
+
+% frequency x label
+ha.Children(4).FontSize = FontSize;
+
+end
