e0946c3f4f4c93fc5261e03c1eaf032accb8e3c2
Daniel R. Herber <daniel.herber@colostate.edu>
2025-03-02 11:17:58 -0700

    Updates to ex_specifications.m

--- 2-linear-control-design/ex_specifications.m
+++ 2-linear-control-design/ex_specifications.m
@@ -1,15 +1,19 @@
 % ex_specifications.m
-% illustration of the time and frequency domain specifications using Matlab
-% functions stepinfo, dcgain, allmargin, and loopsens as well as Nyquist
-% diagrams
+% illustration of the time- and frequency-domain specifications using
+% Matlab functions stepinfo, dcgain, allmargin, and loopsens as well as
+% Nyquist diagrams
+% [course] Session 6 - Linear Control (3)
 close all; clear; clc
 
+%--------------------------------------------------------------------------
+% original system
+%--------------------------------------------------------------------------
 % system matrices
 A = [-0.14 0.33 -0.33; 0.1 -0.28 0; 0 1.7 -0.77];
 B = [0; 0; -0.025];
 C = [2 0 0];
 
-% state-space model
+% LTI state-space model
 sys = ss(A,B,C,[]);
 
 % rise time, settling time, and other step-response characteristics
@@ -17,7 +21,7 @@ S = stepinfo(sys)
 k = dcgain(sys)
 
 %--------------------------------------------------------------------------
-% full state feedback
+% full-state feedback
 %--------------------------------------------------------------------------
 % desired eigenvalues
 epsilon = [0 1e-4 -1e-4];
@@ -34,7 +38,7 @@ S1 = stepinfo(sys1)
 k1 = dcgain(sys1)
 
 %--------------------------------------------------------------------------
-% full state feedback with integrator
+% full-state feedback with integrator
 %--------------------------------------------------------------------------
 % augmented system matrices
 Ai = [-0.14 0.33 -0.33 0;
@@ -63,23 +67,30 @@ ki = dcgain(sysi)
 %--------------------------------------------------------------------------
 % gain margin, phase margin, and crossover frequencies
 %--------------------------------------------------------------------------
-rng(6848)
-% rng(4456434) % <- also try this
-P = rss(4); % generate a randomized continuous-time state-space model
-Sm = allmargin(P)
+% generate a randomized continuous-time LTI state-space model
+rng(23525)
+% rng(2652526) % <- also try
+% rng(973724) % <- also try
+P = rss(4);
+
+% calculate various margins
+Sm = allmargin(P);
+Sm.Stable
 
 % plot Bode frequency response
-hf = figure; hf.Color = 'w';
+figure;
 bode(P)
+plot_example_bode1(P,Sm) % see function below
 
 % plot Nyquist frequency response
-hf = figure; hf.Color = 'w';
+figure;
 nyquist(P)
+plot_example_nyquist % see function below
 
 %--------------------------------------------------------------------------
 % sensitivity functions of plant-controller feedback loop
 %--------------------------------------------------------------------------
-% create a state-space model
+% create a LTI state-space model
 rng(242)
 P = rss(6);
 C = rss(3);
@@ -88,5 +99,149 @@ C = rss(3);
 SF = loopsens(P,C)
 
 % plot sensitivity at the plant input
-hf = figure; hf.Color = 'w';
+figure
 bode(SF.Si)
+plot_example_bode2 % see function below
+
+%--------------------------------------------------------------------------
+% plotting code
+% (not the main content)
+function plot_example_bode1(P,Sm)
+
+% colors and other parameters
+niceblue = [77, 121, 167]/255;
+nicered = [225, 86, 86]/255;
+nicegreen = [109, 195, 80]/255;
+LineWidth = 1;
+MarkerSize = 12;
+FontSize = 12;
+plotOpts = {'LineWidth',LineWidth,'MarkerSize',MarkerSize};
+
+% figure
+hf = gcf; hf.Color = 'w'; hold on
+
+% axis handles
+ha = gca; ha1 = ha.Children(1); ha2 = ha.Children(2);
+
+% magnitude plot
+ha1.LineWidth = LineWidth;
+ha1.XColor = 'k';
+ha1.YColor = 'k';
+ha1.FontSize = FontSize;
+ha1.Children(1).LineWidth = LineWidth;
+ha1.Children(1).Color = nicered;
+
+% phase plot
+ha2.LineWidth = LineWidth;
+ha2.XColor = 'k';
+ha2.YColor = 'k';
+ha2.FontSize = FontSize;
+ha2.Children(1).LineWidth = LineWidth;
+ha2.Children(1).Color = nicered;
+
+% frequency x label
+ha.Children(4).FontSize = FontSize;
+
+% plot points for gain margins
+if ~isempty(Sm.GMFrequency)
+[mag,phase,w] = bode(P,Sm.GMFrequency);
+mag = mag2db(mag);
+plot(ha1,w,squeeze(mag(:,:,:)),'.',plotOpts{:},'Color',nicegreen)
+plot(ha2,w,squeeze(phase(:,:,:)),'.',plotOpts{:},'Color',nicegreen)
+end
+
+% plot points for phase margins
+if ~isempty(Sm.PMFrequency)
+[mag,phase,w] = bode(P,Sm.PMFrequency);
+mag = mag2db(mag);
+plot(ha1,w,squeeze(mag(:,:,:)),'.',plotOpts{:},'Color',niceblue)
+plot(ha2,w,squeeze(phase(:,:,:)),'.',plotOpts{:},'Color',niceblue)
+end
+
+end
+
+%--------------------------------------------------------------------------
+% plotting code
+% (not the main content)
+function plot_example_nyquist
+
+% colors and other parameters
+niceblue = [77, 121, 167]/255;
+nicered = [225, 86, 86]/255;
+LineWidth = 1;
+FontSize = 12;
+
+% figure
+hf = gcf; hf.Color = 'w'; hold on
+
+grid on
+
+% axis handles
+ha = gca;
+
+% customize main axis
+ha1 = ha.Children(1);
+ha1.LineWidth = LineWidth;
+ha1.XColor = 'k';
+ha1.YColor = 'k';
+ha1.FontSize = FontSize;
+ha1.Children(1).LineWidth = LineWidth;
+ha1.Children(1).Color = nicered;
+ha1.Children(2).FaceColor = nicered;
+ha1.Children(2).EdgeColor = nicered;
+ha1.Children(3).FaceColor = nicered;
+ha1.Children(3).EdgeColor = nicered;
+ha1.Children(6).MarkerEdgeColor = niceblue;
+
+% customize axis labels and title
+ha2 = ha.Children(2);
+ha2.Color = 'k';
+ha2.FontSize = FontSize;
+
+ha3 = ha.Children(3);
+ha3.Color = 'k';
+ha3.FontSize = FontSize;
+
+ha4 = ha.Children(4);
+ha4.Color = 'k';
+ha4.FontSize = FontSize;
+
+end
+
+%--------------------------------------------------------------------------
+% plotting code
+% (not the main content)
+function plot_example_bode2
+
+% colors and other parameters
+nicered = [225, 86, 86]/255;
+LineWidth = 1;
+MarkerSize = 12;
+FontSize = 12;
+
+% figure
+hf = gcf; hf.Color = 'w'; hold on
+
+% axis handles
+ha = gca; ha1 = ha.Children(1); ha2 = ha.Children(2);
+
+% magnitude plot
+ha1.LineWidth = LineWidth;
+ha1.XColor = 'k';
+ha1.YColor = 'k';
+ha1.FontSize = FontSize;
+ha1.Children(1).LineWidth = LineWidth;
+ha1.Children(1).Color = nicered;
+
+% phase plot
+ha2.LineWidth = LineWidth;
+ha2.XColor = 'k';
+ha2.YColor = 'k';
+ha2.FontSize = FontSize;
+ha2.Children(1).LineWidth = LineWidth;
+ha2.Children(1).Color = nicered;
+
+% frequency x label
+ha.Children(4).FontSize = FontSize;
+
+end
