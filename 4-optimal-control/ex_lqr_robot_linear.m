92159573221c2353bdad19f7ac6e1fbe710d8770
Daniel R. Herber <daniel.herber@colostate.edu>
2025-04-04 20:53:55 -0600

    Updates to ex_lqr_robot_linear.m

--- 4-optimal-control/ex_lqr_robot_linear.m
+++ 4-optimal-control/ex_lqr_robot_linear.m
@@ -1,11 +1,13 @@
 % ex_lqr_robot_linear.m
 % determination of the linear model and infinite-horizon LQR gain matrix
-% for Example 5.6 Steady State LQR for the Two Link Robot in LSC with
-% linear simulations of the results
-% *also see Example 4.14 Two Link Robot Arm Control with Multiple
+% for two-link robot example below in LSC with linear simulations of the
+% results
+% [reference] Example 5.6 Steady State LQR for the Two Link Robot in LSC
+% [reference] Example 4.14 Two Link Robot Arm Control with Multiple
 % Integrators in LSC where the pole placement gain matrix is determined
-% *also see Example 2.10 Two Link Robot Arm in LSC where the nonlinear and
-% linear model is described
+% [reference] Example 2.10 Two Link Robot Arm in LSC where the nonlinear
+% and linear model is described
+% [course] Session 10 - Optimal Control (2)
 close all; clear; clc
 
 %--------------------------------------------------------------------------
@@ -44,17 +46,11 @@ Bc = [B; zeros(ny,nu)];
 Br = [zeros(nx,ny);eye(ny)];
 Cc = [C,zeros(ny)];
 
-% initialize figure
-hf = figure; hf.Color = 'w'; hold on
-xlabel('Time [sec]')
-ylabel('Joint Angle [rad]')
-
-% plot reference
-plot(T,r'+Cc*X0_stationary','k--')
+% calculate actual reference
+Ractual = r'+Cc*X0_stationary';
 
 %--------------------------------------------------------------------------
-% pole placement gain matrix from Example 4.14 Two Link Robot Arm Control
-% with Multiple Integrators in LSC
+% pole placement gain matrix from Example 4.14 in LSC
 %--------------------------------------------------------------------------
 % desired eigenvalues
 P = [-7+1i*7, -7-1i*7, -8.6+1i*5, -8.6-1i*5, -9.7+1i*2.6, -9.7-1i*2.6];
@@ -65,16 +61,14 @@ Kc = place(Ac,Bc,P);
 % closed-loop system
 sys = ss(Ac-Bc*Kc,Br,Cc,[]);
 
-% linear simulation
-% NOTE: for the NONLINEAR simulation, see ex_lqr_robot_simulink.slx
+% linear simulation (NOTE: for NONLINEAR sim, see ex_lqr_robot_simulink.slx)
 Y = lsim(sys,r,T,X0);
 
-% plot outputs from pole placement simulation
-plot(T,Y'+Cc*X0_stationary')
+% outputs from linear pole placement simulation
+Ypp = Y'+Cc*X0_stationary';
 
 %--------------------------------------------------------------------------
-% infinite-horizon LQR gain matrix from Example 5.6 Steady State LQR for
-% the Two Link Robot in LSC
+% infinite-horizon LQR gain matrix from Example 5.6 in LSC
 %--------------------------------------------------------------------------
 % state weighting matrix
 Q = zeros(6);
@@ -102,22 +96,78 @@ R = rho*R;
 % closed-loop system
 sys = ss(Ac-Bc*Klqr,Br,Cc,[]);
 
-% linear simulation
-% NOTE: for the NONLINEAR simulation, see ex_lqr_robot_simulink.slx
+% linear simulation (NOTE: for NONLINEAR sim, see ex_lqr_robot_simulink.slx)
 Y = lsim(sys,r,T,X0);
 
-% plot outputs from lqr solution
-plot(T,Y'+Cc*X0_stationary','linewidth',2)
+% outputs from linear lqr simulation
+Ylqr = Y'+Cc*X0_stationary';
 
-% add legend legends
-legend('r_1','r_2','\theta_{1,PP}','\theta_{2,PP}','\theta_{1,LQR}',...
-    '\theta_{2,LQR}','location','best')
+% plot angles
+plot_example(T,Ractual,Ypp,Ylqr)
 
-%--------------------------------------------------------------------------
 % plot closed-loop poles with both approaches
+plot_example_poles(P,CLP)
+
 %--------------------------------------------------------------------------
+% plotting code for angles
+% (not the main content)
+function plot_example(T,R,Ypp,Ylqr)
+
+% colors and other parameters
+niceblue = [77, 121, 167]/255;
+nicered = [225, 86, 86]/255;
+nicegreen = [109, 195, 80]/255;
+nicegray = [170, 170, 170]/255;
+nicepink = [239, 142, 219]/255;
+LineWidth = 1;
+MarkerSize = 12;
+FontSize = 12;
+plotOpts = {'LineWidth',LineWidth,'MarkerSize',MarkerSize};
+
+% initialize figure for plotting theta and r states
 hf = figure; hf.Color = 'w'; hold on
-xlabel('Real'); ylabel('Imag');
-plot(real(P),imag(P),'*')
-plot(real(CLP),imag(CLP),'o')
-legend('poles with pole placement','poles with lqr')
+
+plot(T,rad2deg(R(1,:)),'--',plotOpts{:},'Color',nicegray,'DisplayName','$r_1$')
+plot(T,rad2deg(Ypp(1,:)),'-',plotOpts{:},'Color',niceblue,'DisplayName','$\theta_{1,PP}$')
+plot(T,rad2deg(Ylqr(1,:)),'-',plotOpts{:},'Color',nicered,'DisplayName','$\theta_{1,LQR}$')
+
+plot(T,rad2deg(R(2,:)),'--',plotOpts{:},'Color',nicegray,'DisplayName','$r_2$')
+plot(T,rad2deg(Ypp(2,:)),'-',plotOpts{:},'Color',nicegreen,'DisplayName','$\theta_{2,PP}$')
+plot(T,rad2deg(Ylqr(2,:)),'-',plotOpts{:},'Color',nicepink,'DisplayName','$\theta_{2,LQR}$')
+
+xlabel('Time [sec]')
+ylabel('Joint Angle [deg]')
+
+ha = gca; ha.XColor = 'k'; ha.YColor = 'k'; ha.LineWidth = 1; ha.FontSize = FontSize;
+
+hl = legend(); hl.Location = "best"; hl.FontSize = FontSize; hl.Interpreter = 'latex';
+
+end
+
+%--------------------------------------------------------------------------
+% plotting code for poles
+% (not the main content)
+function plot_example_poles(P,CLP)
+
+% colors and other parameters
+niceblue = [77, 121, 167]/255;
+nicered = [225, 86, 86]/255;
+LineWidth = 1;
+MarkerSize = 12;
+FontSize = 12;
+plotOpts = {'LineWidth',LineWidth,'MarkerSize',MarkerSize};
+
+% initialize figure for plotting theta and r states
+hf = figure; hf.Color = 'w'; hold on
+
+plot(real(P),imag(P),'*',plotOpts{:},'Color',niceblue,'DisplayName','poles with pole placement')
+plot(real(CLP),imag(CLP),'o',plotOpts{:},'Color',nicered,'DisplayName','poles with lqr')
+
+xlabel('Real')
+ylabel('Imag')
+
+ha = gca; ha.XColor = 'k'; ha.YColor = 'k'; ha.LineWidth = 1; ha.FontSize = FontSize;
+
+hl = legend(); hl.Location = "best"; hl.FontSize = FontSize;
+
+end
