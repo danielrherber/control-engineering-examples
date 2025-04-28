4bef875f5a62c4042b96dcffaa3431729615d26c
Daniel R. Herber <daniel.herber@colostate.edu>
2025-04-28 14:50:34 -0600

    Updates to ex_Hinfinity.m

--- 5-robust-stochastic-control/ex_Hinfinity.m
+++ 5-robust-stochastic-control/ex_Hinfinity.m
@@ -1,11 +1,14 @@
 % ex_Hinfinity.m
-% Synthesize a H-infinity optimal controller controller using different
+% synthesize a H-infinity optimal controller controller using different
 % target performance levels. The plant in this example is based on the
 % augmented plant model used in Robust Control of an Active Suspension
-% www.mathworks.com/help/robust/ref/lti.hinfsyn.html
+% [reference] www.mathworks.com/help/robust/ref/lti.hinfsyn.html
+% [course] Session 13 - Control under Uncertainty (2)
 close all; clear; clc
 
-% load the plant
+% load the example plant
+mdl = 'robust/HinfinityControllerSynthesisExample';
+openExample(mdl)
 load hinfsynExData P
 
 % get size of the plant
@@ -23,8 +26,8 @@ nmeas = 2;
 size(K1)
 
 % the optimal performance level achieved by this controller is returned as
-% gamma. This value is the H-infinity norm of the closed-loop system CL
-% between W -> Z
+% gamma. This value is the H-infinity norm of the closed-loop system
+% between V -> Ys
 gamma
 
 % get original transfer function between W -> Z
@@ -37,6 +40,49 @@ Hinf_CL = mag2db(hinfnorm(CL))
 % plot singular values of the systems along with achieved gamma
 hf = figure; hf.Color = 'w';
 Gamma = ss(gamma);
+plot_example(P_original,CL,Gamma)
+
+%--------------------------------------------------------------------------
+% plotting code
+% (not the main content)
+function plot_example(P_original,CL,Gamma)
+
+% colors and other parameters
+niceblue = [77, 121, 167]/255;
+nicered = [225, 86, 86]/255;
+nicegray = [170, 170, 170]/255;
+LineWidth = 1;
+FontSize = 12;
+
+% initialize figure
+hf = figure; hf.Color = 'w'; hold on
+
 sigma(P_original,CL,Gamma)
+
 ylim([-60,10])
-legend
+
+ha = gca; ha.AxesStyle.RulerColor = 'k';
+ha.AxesStyle.GridColor = 'k';
+ha.AxesStyle.FontSize = FontSize;
+
+ha.YLabel.Color = 'k';
+ha.YLabel.FontSize = FontSize;
+
+ha.XLabel.Color = 'k';
+ha.XLabel.FontSize = FontSize;
+
+ha.Title.Color = 'k';
+ha.Title.FontSize = FontSize;
+
+ha.Responses(1).Color = niceblue;
+ha.Responses(1).LineWidth = LineWidth;
+
+ha.Responses(2).Color = nicered;
+ha.Responses(2).LineWidth = LineWidth;
+
+ha.Responses(3).Color = nicegray;
+ha.Responses(3).LineWidth = LineWidth;
+
+hl = legend(); hl.Location = "best"; hl.FontSize = FontSize;
+
+end
